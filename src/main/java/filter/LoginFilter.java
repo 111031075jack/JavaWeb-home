510a2e56b206eab25b6babfe0f9e02ddf523307e
111031075jack <111031075@live.asia.edu.tw>
2025-07-31 01:26:31 +0800

    update

---
+++ src/main/java/filter/LoginFilter.java
@@ -0,0 +1,35 @@
+package filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+// 有登入才放行的過濾器
+@WebFilter(urlPatterns = {"/user", "/user/delete"})
+//@WebFilter(urlPatterns = {"/user/*"})
+public class LoginFilter extends HttpFilter {
+
+	@Override
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		// 判斷是否有登入 ?
+		HttpSession session = request.getSession(false);
+		if(session == null || session.getAttribute("username") == null) {
+			request.setAttribute("message", "請先登入");
+			request.getRequestDispatcher("/WEB-INF/view/result.jsp").forward(request, response);
+			return;
+		}
+		// 放行
+		chain.doFilter(request, response);
+		
+		
+	}
+	
+}
