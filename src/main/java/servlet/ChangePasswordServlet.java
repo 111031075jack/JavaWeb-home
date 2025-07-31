3c7ff8fdc38e2700e170b833be31a1e252339f36
111031075jack <111031075@live.asia.edu.tw>
2025-07-31 14:03:27 +0800

    update

--- src/main/java/servlet/ChangePasswordServlet.java
+++ src/main/java/servlet/ChangePasswordServlet.java
@@ -13,8 +13,8 @@ public class ChangePasswordServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		
+		// 重導一個 change_password.jsp 頁面
+		req.getRequestDispatcher("/WEB-INF/view/change_password.jsp").forward(req, resp);
 		
 	}
 	
