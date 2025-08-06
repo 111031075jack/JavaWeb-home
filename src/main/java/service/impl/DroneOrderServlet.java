5c62a222739beda3b403434f199c7001028399a1
111031075jack <111031075@live.asia.edu.tw>
2025-08-06 16:13:22 +0800

    update

--- src/main/java/service/impl/DroneOrderServlet.java
+++ src/main/java/service/impl/DroneOrderServlet.java
@@ -1,20 +1,37 @@
 package service.impl;
 
 import java.io.IOException;
+import java.util.List;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.DroneOrder;
+import service.DroneOrderService;
 
 @WebServlet("/drone/order")
 public class DroneOrderServlet extends HttpServlet {
 	
+	DroneOrderService service = new DroneOrderServiceImpl();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		
+		// 參數取得
+		String orderByName = req.getParameter("orderByName");
+		String asc = req.getParameter("asc");
+		// 分析參數
+		// 取得傳入訂單資料
+		if(orderByName == null) {
+			orderByName = "order_id";
+		}
+		asc = (asc == null) ? "true" : asc;
+		// 取得訂單資料
+		List<DroneOrder> droneOrders = service.findAll(orderByName, Boolean.parseBoolean(asc));
+		req.setAttribute("droneOrders", droneOrders);
+		// 重導到 /WEB-INF/view/drone_order.jsp
+		req.getRequestDispatcher("/WEB-INF/view/drone_order.jsp").forward(req, resp);
 		
 	}
 	
